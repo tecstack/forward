4ae5ac1769da251462ef05a9bf322665cbc2ac08
Azrael <13802880354@139.com>
2018-05-15 14:54:02 +0800

    unittest: + copyright notice + license notice

--- unittests/testC2960.py
+++ unittests/testC2960.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python
-# encodint:utf-8
-# Author:Cheung Kei-Chuen
+# (c) 2015-2018, Wang Zhe <azrael-ex@139.com>, Zhang Qi Chuan <zhangqc@fits.com.cn>
+#
+# This file is part of Ansible
+#
+# Forward is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Forward is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import unittest
 import importlib
 from forward.utils.forwardError import ForwardError
@@ -49,4 +63,3 @@ class deviceClassC2960(unittest.TestCase):
         cls = getattr(importlib.import_module('forward.devclass.{dev}'.format(dev=self.deviceClassName)),
                       self.deviceClassName.upper())
         self.assertEquals(cls.__bases__[0], BASECISCO)
-
