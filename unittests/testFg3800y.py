33fb89c818775053d6a2eba4c32fb8efceb3e71d
linzhigeng <lzg397604336@163.com>
2020-03-12 17:22:34 +0800

    add unittests , update version to 3.3.3

---
+++ unittests/testFg3800y.py
@@ -0,0 +1,65 @@
+# (c) 2015-2018, Wang Zhe <azrael-ex@139.com>, Zhang Qi Chuan <zhangqc@fits.com.cn>
+#
+# This file is part of Ansible
+#
+# Forward is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Forward is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import importlib
+from forward.utils.forwardError import ForwardError
+from forward.devclass.baseFortinet import BASEFORTINET
+
+
+class deviceClassFg3800(unittest.TestCase):
+    def setUp(self):
+        self.deviceClassName = "fg3800"
+        self.initParameters = ["ip",
+                               "username",
+                               "password",
+                               "port",
+                               "timeout",
+                               "privilegePw",
+                               "isLogin",
+                               "isEnable",
+                               "channel",
+                               "shell",
+                               "basePrompt",
+                               "prompt",
+                               "moreFlag"]
+        self.baseClassMethod = ["login",
+                                "logout",
+                                "execute",
+                                "getMore",
+                                "getPrompt",
+                                "cleanBuffer"]
+
+    def test_class_parameters(self):
+        _dev = getattr(importlib.import_module('forward.devclass.{dev}'.format(dev=self.deviceClassName)),
+                       self.deviceClassName.upper())
+        for parameter in self.initParameters:
+            if not hasattr(_dev(1,2,3), parameter):
+                raise IOError('%s not have parameter:' % (self.deviceClassName), parameter)
+
+    def test_base_class_method(self):
+        _dev = getattr(importlib.import_module('forward.devclass.{dev}'.format(dev=self.deviceClassName)),
+                       self.deviceClassName.upper())
+        for method in self.baseClassMethod:
+            if not hasattr(_dev(1,2,3), method):
+                raise IOError('%s not have parameter:' % (self.deviceClassName), method)
+
+    def test_inherit_check(self):
+        # Inherit from BASESSHV2
+        cls = getattr(importlib.import_module('forward.devclass.{dev}'.format(dev=self.deviceClassName)),
+                      self.deviceClassName.upper())
+        self.assertEquals(cls.__bases__[0], BASEFORTINET)
